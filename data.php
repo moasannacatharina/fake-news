f84b00e1e354554b685e55c3195d0b3a50f3f16a
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2020-10-19 10:03:29 +0200

    Update data.php

--- data.php
+++ data.php
@@ -4,3 +4,11 @@ declare(strict_types=1);
 
 // This is the file where you can keep your data arrays such as articles and
 // authors.
+
+$authors = [
+    ['name' => 'Maja Namnsson', 'id' => 1],
+    ['name' => 'Olle Trens', 'id' => 2],
+    ['name' => 'Moa Berg', 'id' => 3],
+    ['name' => 'Kalle Trollsson', 'id' => 4],
+    ['name' => 'Bo Berntsson', 'id' => 5]
+];
